5bed62d9f0c5f8b19c8a7a450d995ef241c87b25
Clarissa de Cássia <clari.developer@gmail.com>
2023-06-30 14:47:46 -0300

    adicionado JS

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const btnNovoConselho = document.querySelector('.btn-novo-conselho');
+const numeroConselho = document.getElementById('id-conselho')
+const novoConselho = document.querySelector('.conselho')
+const urlBase = 'https://api.adviceslip.com/advice';
+
+btnNovoConselho.addEventListener("click", gerarConselhosAleatorios)
+
+async function geraIdConselhos() {
+
+    const resposta = await fetch(urlBase);
+    const conselhos = await resposta.json();
+    return conselhos.slip.id
+
+}
+
+async function gerarConselhosAleatorios() {
+
+    const idConselho = await geraIdConselhos();
+    numeroConselho.innerHTML = `ADVICE #${idConselho}`
+
+    const resposta = await fetch(`${urlBase}/${idConselho}`);
+    const conselho = await resposta.json();
+    novoConselho.innerHTML = conselho.slip.advice
+}
+
+gerarConselhosAleatorios()
