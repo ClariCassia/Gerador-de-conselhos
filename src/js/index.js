cf2c7239ef7e27eb63dd93143c9a2c1b72ab1793
Clarissa de Cassia <clari.developer@gmail.com>
2023-11-05 17:38:24 -0300

    realizada identação

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ const btnNovoConselho = document.querySelector('.btn-novo-conselho');
 let idConselho = document.getElementById('id-conselho')
 let conselho = document.querySelector('.conselho')
 
-
 async function gerarConselhosAleatorios() {
   const resposta = await fetch('https://api.adviceslip.com/advice')
   const infoConselho = await resposta.json();   
